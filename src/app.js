1ddd33df88452208d15b36bc2149223e9e9b1ca5
kimdadaist <kim.dadaist@gmail.com>
2025-05-25 18:33:53 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>Edit src/App.js and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
